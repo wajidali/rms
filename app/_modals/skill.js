3add5105e642e4d31f11436b13a69daaa9b9540c
wajidali <wajidali.khilji@gmail.com>
2017-06-11 13:21:25 +0300

    form changes

---
+++ app/_modals/skill.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Skill = (function () {
+    function Skill(value, name) {
+        this.value = value;
+        this.name = name;
+    }
+    return Skill;
+}());
+exports.Skill = Skill;
+//# sourceMappingURL=skill.js.map
